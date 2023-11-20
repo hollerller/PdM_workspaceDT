71893324521a837d4a6ced892a7a34dea62dd75f
hollerller <hollerller@gmail.com>
2023-11-20 00:12:05 -0500

    Practica 5 sin comentarios

---
+++ practica5/Drivers/API/Inc/API_debounce.h
@@ -0,0 +1,20 @@
+/*
+ * API_debounce.h
+ *
+ *  Created on: Nov 12, 2023
+ *      Author: hollerller
+ */
+
+#include "API_delay.h"
+
+#ifndef API_INC_API_DEBOUNCE_H_
+#define API_INC_API_DEBOUNCE_H_
+
+
+void debounceFSM_init();	// Load the initial state of the FMS
+void debounceFSM_update();	// Updates the state of the FMS
+
+bool_t readKey();			// Returns true or false if the key is pressed
+
+
+#endif /* API_INC_API_DEBOUNCE_H_ */
---
+++ practica5/README.md
@@ -0,0 +1,11 @@
+# Practica 5
+
+Implementar un módulo de software sencillo para trabajar con la UART
+
+## Descripción
+
+Sobre el ejercicio de la práctica 4, implementar un programa que utilice el módulo de uart para informar al usuario cuando se producen los flancos ascendente y descendente.
+
+## Autor
+
+- Nombre: David Tobausra
